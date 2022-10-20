eae569c56a31abf6399ff51b6a288e6b935b6796
Riley.Morris <rileymichaelmorris@gmail.com>
2022-10-20 19:39:56 -0400

    fix Math.random ---> Math.random()

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,7 +1,6 @@
 let choices = ['rock', 'paper', 'scissors']
 function genCompChoice() {
-    let compChoice = choices[Math.floor(Math.random * choices.length)];
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];
     console.log(compChoice)
 }
 genCompChoice()
-    
