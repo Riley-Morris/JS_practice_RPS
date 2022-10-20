d139d9fa29bcc5201e8a2ff3d29dcc17ba238275
Riley.Morris <rileymichaelmorris@gmail.com>
2022-10-20 19:37:44 -0400

    add initial files, begin writing js

---
+++ RockPaperScissors.js
@@ -0,0 +1,7 @@
+let choices = ['rock', 'paper', 'scissors']
+function genCompChoice() {
+    let compChoice = choices[Math.floor(Math.random * choices.length)];
+    console.log(compChoice)
+}
+genCompChoice()
+    
