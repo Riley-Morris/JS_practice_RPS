967737f768d0b4f6e332141e49ce0b107046f507
Riley.Morris <rileymichaelmorris@gmail.com>
2022-10-20 20:44:47 -0400

    fix embarassing logic bug, wrote parts of multiple statements in python not js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -8,14 +8,20 @@ function genUserChoice() {
     let userChoice = prompt('Please enter "rock", "paper", "or scissors"').toLowerCase();
     return userChoice;
 }
-let cChoice = genCompChoice();
-let pChoice = genUserChoice();
+let cChoice;
+let pChoice;
+let pWinOrLose;
+let cWinOrLose;
 
 function resolveGame(playerChoice, computerChoice) {
-    if (pChoice != 'rock' || pChoice != 'scissors' || pChoice != 'paper'){
+    
+
+    if (playerChoice !== 'rock' && playerChoice !== 'scissors' && playerChoice !== 'paper'){
+       
         console.log('please enter only "rock", "paper", "or scissors" exactly. Im too dumb to parse the information ');
+        console.log(playerChoice);
         let pChoice = genUserChoice();
-        resolveGame()
+        resolveGame(pChoice, computerChoice)
     }else if (playerChoice === computerChoice) {
         console.log(`You have both selected ${playerChoice}, please play again!`);
     }else if (playerChoice === 'scissors' && computerChoice === 'rock') {
@@ -36,9 +42,18 @@ function resolveGame(playerChoice, computerChoice) {
 
 function loseScreen() {
     console.log(`You have lost with ${pChoice}, computer won with ${cChoice} please play again!`);
+    cWinOrLose += 1
 }
 function winScreen() {
     console.log(`You have won with ${pChoice}, computer lost with ${cChoice} please play again!`);
+    pWinOrLose += 1
 }
-
-resolveGame(pChoice, cChoice)
+function game() {
+    for (let i = 1; i<6; i++) {
+    cChoice = genCompChoice();
+    pChoice = genUserChoice();
+    resolveGame(pChoice, cChoice)
+    console.log(`score is PLAYER: ${pWinOrLose}, COMPUTER: ${cWinOrLose}`)
+    }
+}
+game()
